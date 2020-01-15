4514ce34de05764f0d47cee1c3ec4dd59037dc7c
Ryu <r@ryunomacbook-puro.local>
2020-01-15 13:13:43 +0900

    Initial Commit

---
+++ FirstKadaiApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FirstKadaiApp
+//
+//  Created by Ryu on 2020/01/15.
+//  Copyright © 2020年 Ryu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
